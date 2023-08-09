86eed579481416c5c16925d0370f04aee533697a
Denis Luiz Massola <dluiz-ma@c2r9p5.42sp.org.br>
2023-08-09 17:30:25 -0300

    2023_08_09_v2

---
+++ d04_sem_repo/my_script.sh
@@ -0,0 +1,20 @@
+# configuração e instalação do DJANGO
+DIRETORIO_AMBIENTE="django_venv"
+
+python3 -m venv $DIRETORIO_AMBIENTE
+
+. $DIRETORIO_AMBIENTE/bin/activate
+
+python3 -m pip install --force-reinstall -r requirements.txt
+
+# Criação do projeto
+django-admin startproject d04 .
+
+# Criação da aplicação
+django-admin startapp ex00
+
+django-admin startapp ex01
+
+django-admin startapp ex02
+
+django-admin startapp ex03
