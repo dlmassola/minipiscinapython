d40d70d4ea5c71da423de9883d343f2477077d90
Denis Luiz Massola <dluiz-ma@c2r9p5.42sp.org.br>
2023-08-10 13:40:00 -0300

    2023_08_10_v1

--- d04_sem_repo/ex03/views.py
+++ d04_sem_repo/ex03/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+def generate_shades():
+    shades = [
+        '#' + ''.join(random.choice('0123456789ABCDEF') for _ in range(6))
+        for _ in range(4)
+    ]
+    return shades
+
+def table(request):
+    shades = generate_shades()
+    
+    step = 255 / 50
+    
+    context = {
+        'shades': shades,
+        "range": [
+            "{:02X}".format(int(i * step)) for i in range(50)
+        ]
+    }
+    return render(request, 'ex03/table.html', context)
--- d04_sem_repo/my_script.sh
+++ d04_sem_repo/my_script.sh
@@ -1,11 +1,11 @@
+python3 -m pip install --force-reinstall -r requirements.txt
+
 # configuração e instalação do DJANGO
 DIRETORIO_AMBIENTE="django_venv"
 
 python3 -m venv $DIRETORIO_AMBIENTE
 
 . $DIRETORIO_AMBIENTE/bin/activate
-
-python3 -m pip install --force-reinstall -r requirements.txt
 
 # Criação do projeto
 django-admin startproject d04 .
